96475f508393c6c69f8f3e827e8910a2baf4a807
EmerMine <2531852169@qq.com>
2026-08-07 14:46:35 +0800

    修改欢迎窗口

--- main.py
+++ main.py
@@ -30,29 +30,29 @@ def main():
         msg_box.setText(
             "<h3>欢迎</h3>"
             "<p>欢迎使用「希沃批注替换」！</p>"
-            "<p>使用本程序前，请先确保您的计算机上安装了以下任意软件：</p>"
+            "<p>使用本程序前，建议在您的计算机上安装以下任意软件：</p>"
             "<ul>"
-            "<li>「Ink Canvas Artistry (ICA)」任意版本</li>"
-            "<li>「InkCanvasForClass (ICC)」任意版本</li>"
+            # "<li>「Ink Canvas Artistry (ICA)」任意版本</li>"
+            # "<li>「InkCanvasForClass (ICC)」任意版本</li>"
             "<li>「InkCanvasForClass Community Edition (ICC-CE)」1.7.18.7+</li>"
             "</ul>"
             "您可以单击下方按钮前往 Github 下载。</p>"
         )
-        btn_ica_website = QPushButton("ICA Github")
-        btn_icc_website = QPushButton("ICC Github")
+        # btn_ica_website = QPushButton("ICA Github")
+        # btn_icc_website = QPushButton("ICC Github")
         btn_icc_ce_website = QPushButton("ICC-CE Github")
-        msg_box.addButton(btn_ica_website, QMessageBox.AcceptRole) # type: ignore
-        msg_box.addButton(btn_icc_website, QMessageBox.AcceptRole) # type: ignore
+        # msg_box.addButton(btn_ica_website, QMessageBox.AcceptRole) # type: ignore
+        # msg_box.addButton(btn_icc_website, QMessageBox.AcceptRole) # type: ignore
         msg_box.addButton(btn_icc_ce_website, QMessageBox.AcceptRole) # type: ignore
         ok_btn = msg_box.addButton("OK", QMessageBox.AcceptRole) # type: ignore
         msg_box.setDefaultButton(ok_btn)
         
-        btn_ica_website.clicked.connect(
-            lambda: webbrowser.open("https://github.com/InkCanvas/Ink-Canvas-Artistry")
-        )
-        btn_icc_website.clicked.connect(
-            lambda: webbrowser.open("https://github.com/InkCanvas/InkCanvasForClass")
-        )
+        # btn_ica_website.clicked.connect(
+        #     lambda: webbrowser.open("https://github.com/InkCanvas/Ink-Canvas-Artistry")
+        # )
+        # btn_icc_website.clicked.connect(
+        #     lambda: webbrowser.open("https://github.com/InkCanvas/InkCanvasForClass")
+        # )
         btn_icc_ce_website.clicked.connect(
             lambda: webbrowser.open("https://github.com/InkCanvasForClass/community")
         )
