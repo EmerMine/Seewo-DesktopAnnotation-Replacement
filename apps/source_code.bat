31ba2a09a76c315d5e7cc7175465b49b512132ff
EmerMine <2531852169@qq.com>
2026-09-19 16:05:52 +0800

    fix: 修复批处理文件名拼写错误并更新exe哈希值
    1. 修正了配置文件和代码中的"Deskop"拼写为"Desktop"
    2. 更新了DesktopAnnotation.exe的SHA256校验值
    3. 更新了批处理脚本的调用路径和文件名

--- apps/source_code.bat
+++ apps/source_code.bat
@@ -1,3 +1,39 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFD5ZSRavNWW/OpEZ++Pv4Pq78wMYTOdf
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 :: DesktopAnnotation.exe 的源代码
 
-start "" "C:\Program Files (x86)\Seewo\MiniApps\DesktopAnnotation\Seewo-DeskopAnnotation-Replacement.bat"
+@echo off
+cd /d "%~dp0"
+call "%~dp0Seewo-DesktopAnnotation-Replacement.bat"
+exit /b
